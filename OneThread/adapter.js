ea74f4b55355328f8bc7a9b23d523c7d9b879ca3
Priezt <vegetabird@gmail.com>
2011-11-16 16:00:44 +0800

    add weibo support

--- OneThread/adapter.js
+++ OneThread/adapter.js
@@ -1,9 +1,42 @@
 var adapters = {
 	'weibo': {
+		'icon': 'img/weibo.png',
 		'url': 'http://weibo.com/aj/mblog/fsearch',
 		'login': '',
 		'parser': function(data){
-			return [];
+			//console.log(data);
+			var result = [];
+			var d = $(data.data);
+			//console.log(d);
+			$("#playground").empty();
+			$("#playground").append(d);
+			$("#playground dl.feed_list").each(function(){
+				//console.log("got");
+				var date = $(this).find("p.info a.date").attr("date");
+				var uid = $(this).attr("mid");
+				var link = "http://weibo.com" + $(this).find("p.info a.date").attr("href");
+				var author = $(this).find("dt.face a img");
+				$(this).find("dd.clear").remove();
+				$(this).find(".info").remove();
+				$(this).find("dt.face").remove();
+				$(this).find("a").each(function(){
+					var href = $(this).attr("href");
+					if(href.indexOf("/") == 0){
+						href = "http://weibo.com" + href;
+					}
+					$(this).attr("href", href);
+				}).click(item_link_click);
+				var item = {
+					'content': $(this),
+					'author': author,
+					'date': date,
+					'uid': uid,
+					'link': link
+				};
+				result.push(item);
+			});
+			//console.log(result);
+			return result;
 		}
 	},
 	'kaixin': {
@@ -14,3 +47,13 @@ var adapters = {
 		}
 	}
 };
+
+function item_link_click(event){
+	console.log($(this).attr("href"));
+	event.preventDefault();
+}
+
+function item_open_link_click(event){
+	console.log($(this).attr("href"));
+	event.preventDefault();
+}
--- OneThread/main.js
+++ OneThread/main.js
@@ -4,6 +4,7 @@ var current_feed = '';
 var adpt = '';
 var errors = [];
 var to_be_shown = [];
+var cache = new Array();
 
 $(init);
 
@@ -43,6 +44,7 @@ function load_conf(){
 	}else{
 		console.log('save default conf');
 		conf = {
+			'debug': true,
 			'interval': 5,
 			'interest': ['weibo', 'kaixin']
 		};
@@ -96,7 +98,7 @@ function phase_2_get_url(){
 function phase_3_data_callback(data){
 	console.log("phase 3");
 	var result = adpt.parser(data);
-	if(result){
+	if(result && result.length > 0){
 		console.log("item fetched: " + result.length);
 		var items = [];
 		for(var c=0;c<result.length;c++){
@@ -106,19 +108,52 @@ function phase_3_data_callback(data){
 		}
 		phase_4_show_items(items);
 	}else{
-		// todo: need login notification
-		phase_1_get_adapter();
+		phase_error();
 	}
 }
 
 function phase_4_show_items(items){
 	console.log("phase 4: " + items.length);
-	phase_1_get_adapter();
+	for(var c=0;c<items.length;c++){
+		var item = items[c];
+		var new_div = $("<div></div>")
+			.addClass("ot_item");
+		new_div.append(
+			$("<table border=0></table>")
+				.append(
+					$("<tr></tr>")
+						.append(
+							$("<td></td>")
+								.attr("valign", "top")
+								.addClass("ot_item_left")
+								.append(item.author)
+								.append($("<br>"))
+								.append(
+									$("<a></a>")
+										.attr("href", item.link)
+										.append(
+											$("<img>")
+												.attr("src", adpt.icon)
+										)
+										.click(item_open_link_click)
+								)
+						).append(
+							$("<td></td>")
+								.addClass("ot_item_right")
+								.append(item.content)
+						)
+				)
+		);
+		new_div.insertAfter($("#top_dummy"));
+	}
+	phase_end();
+	//phase_1_get_adapter();
 }
 
 function phase_error(){
 	console.log("phase error: " + current_feed);
 	errors.push((new Date()).toUTCString() + ": " + current_feed);
+	// todo: need login notification
 	phase_1_get_adapter();
 }
 
@@ -127,6 +162,13 @@ function phase_end(){
 }
 
 function is_new_item(item){
-	// todo: filter rule
-	return true;
+	if(! cache[current_feed]){
+		cache[current_feed] = new Array();
+	}
+	if(cache[current_feed][item.uid]){
+		return false;
+	}else{
+		cache[current_feed][item.uid] = true;
+		return true;
+	}
 }
